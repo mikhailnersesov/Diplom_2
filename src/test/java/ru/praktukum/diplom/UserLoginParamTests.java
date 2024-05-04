80d1e0fa89430291388475dc958cd68f80dc2344
Mikhail Nersesov <mikhail.nersesov@gmail.com>
2024-05-04 17:07:23 +0200

    userLogin tests ready

---
+++ src/test/java/ru/praktukum/diplom/UserLoginParamTests.java
@@ -0,0 +1,71 @@
+package ru.praktukum.diplom;
+
+import io.qameta.allure.Description;
+import io.qameta.allure.junit4.DisplayName;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import ru.praktikum.diplom.client.UserClient;
+import ru.praktikum.diplom.step.UserSteps;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.apache.http.HttpStatus.*;
+import static org.hamcrest.Matchers.is;
+
+@RunWith(Parameterized.class)
+public class UserLoginParamTests {
+    protected static List<String> userTokens = new ArrayList();
+    protected static UserSteps userSteps;
+    static String email = "test-data@yandex" + RandomStringUtils.randomAlphabetic(5) + ".ru";
+    static String password = RandomStringUtils.randomAlphabetic(10);
+    static String name = RandomStringUtils.randomAlphabetic(10);
+
+    @AfterClass
+    public static void tearDown() {
+        for (String token : userTokens) {
+            if (token != null) {
+                userSteps.deleteUserRequest(token).statusCode(SC_ACCEPTED).body("message", is("User successfully removed"));
+            }
+        }
+    }
+
+    @BeforeClass
+    public static void setUp() {
+        userSteps = new UserSteps(new UserClient());
+        userSteps
+                .createUserRequest(email, password, name)
+                .statusCode(SC_OK) //BUG: STEBURG-1: should be not 200, but 201 created
+                .body("success", is(true));
+
+    }
+
+    @Parameterized.Parameter(0)
+    static public String emailParam = "test-data@yandex" + RandomStringUtils.randomAlphabetic(5) + ".ru";
+    @Parameterized.Parameter(1)
+    static public String passwordParam = RandomStringUtils.randomAlphabetic(10);
+
+    @Parameterized.Parameters
+    public static Object[][] data() {
+        return new Object[][]{
+                {"test-data@yandex.ru", password},
+                {email, "123"},
+                {"", password},
+                {email, ""},
+                {null, password},
+                {email, null}
+        };
+    }
+
+    @Test
+    @DisplayName("Ошибка при логине с неверными данными")
+    @Description("Данный тест покрывает следующие кейсы: 2) нельзя создать двух одинаковых пользователей; 4) запрос возвращает правильный код ответа(403 Forbidden)")
+    public void loginWithWrongPasswordFailed() {
+        userSteps.loginUserRequest("test-data@yandex.ru", password).statusCode(SC_UNAUTHORIZED).body("message", is("email or password are incorrect"));
+    }
+}
---
+++ src/test/java/ru/praktukum/diplom/UserLoginTests.java
@@ -0,0 +1,55 @@
+package ru.praktukum.diplom;
+
+import io.qameta.allure.Description;
+import io.qameta.allure.junit4.DisplayName;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.Test;
+import ru.praktikum.diplom.client.UserClient;
+import ru.praktikum.diplom.step.UserSteps;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.apache.http.HttpStatus.*;
+import static org.hamcrest.Matchers.is;
+
+public class UserLoginTests {
+    protected static List<String> userTokens = new ArrayList();
+    protected String userToken;
+    protected static UserSteps userSteps;
+    String email = "test-data@yandex" + RandomStringUtils.randomAlphabetic(5) + ".ru";
+    String password = RandomStringUtils.randomAlphabetic(10);
+    String name = RandomStringUtils.randomAlphabetic(10);
+
+    @AfterClass
+    public static void tearDown() {
+        for (String token : userTokens) {
+            if (token != null) {
+                userSteps.deleteUserRequest(token).statusCode(SC_ACCEPTED).body("message", is("User successfully removed"));
+            }
+        }
+    }
+
+    @Before
+    public void setUp() {
+        userSteps = new UserSteps(new UserClient());
+        userSteps
+                .createUserRequest(email, password, name)
+                .statusCode(SC_OK) //BUG: STEBURG-1: should be not 200, but 201 created
+                .body("success", is(true));
+
+    }
+
+    @Test
+    @DisplayName("Успешный логин уникального пользователя с корректными данными")
+    @Description("Данный тест покрывает следующие кейсы: 1) пользователя можно создать; 3) чтобы создать пользователя, нужно передать в ручку все обязательные поля; 4) запрос возвращает правильный код ответа (201 Created); 5) успешный запрос возвращает success: true")
+    public void loginUserSucessfully() {
+        String accessToken = userSteps.loginUserRequest(email, password).statusCode(SC_OK).extract().path("accessToken");
+        int spaceIndex = accessToken.indexOf(" "); // Find the index of the space character
+        userToken = accessToken.substring(spaceIndex + 1);  // Extract the second part of the string using substring
+        userTokens.add(userToken);
+    }
+}
